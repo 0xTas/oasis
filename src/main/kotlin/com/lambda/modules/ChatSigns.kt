b9b3328a95e554615a869e70af0f9ae3eeeb0661
0xTas <admin@0xtas.dev>
2023-06-20 23:50:34 -0700

    choose custom colors for chat messages

--- src/main/kotlin/com/lambda/modules/ChatSigns.kt
+++ src/main/kotlin/com/lambda/modules/ChatSigns.kt
@@ -17,7 +17,7 @@ import com.lambda.client.event.events.RenderWorldEvent
 
 
 /**
- * @author 0xTas [@0xTas] <root@0xTas.dev>
+ * @author Tas [@0xTas] <root@0xTas.dev>
  */
 internal object ChatSigns : PluginModule(
     name = "ChatSigns",
@@ -26,9 +26,11 @@ internal object ChatSigns : PluginModule(
     pluginMain = Oasis
 ){
     private val posSet = hashSetOf<BlockPos>()
-    private val chunks by setting("Chunks", value = 7, range = 1..16, step = 1, description = "Range of chunks to scan in")
+    private val chunks by setting("Chunks", value = 5, range = 1..12, step = 1, description = "Range of chunks to scan in (5 is 2b max)")
     private val showCoords by setting("Show Coordinates", value = true, description = "Include sign coordinates")
     private val oldSigns by setting("Show Old Status", value = true , description = "Annotate signs placed prior to Minecraft version 1.8")
+    private val oldColor by setting("Old Text Color", value = TextColor.GREEN, description = "Text color for old signs")
+    private val chatColor by setting("Chat Text Color", value = TextColor.WHITE, description = "Text color for all other signs")
     private val displayMode by setting("Display Mode", value = DisplayMode.FANCY, description = "Toggle between fancy/compact chat")
 
     private val timer = TickTimer()
@@ -37,6 +39,52 @@ internal object ChatSigns : PluginModule(
         FANCY, COMPACT
     }
 
+    private enum class TextColor {
+        RED, DARK_RED, GREEN, DARK_GREEN,
+        BLUE, DARK_BLUE, AQUA,
+        PURPLE, DARK_PURPLE, GOLD, YELLOW,
+        GRAY, DARK_GRAY, BLACK, WHITE
+    }
+
+
+    private fun color(old: Boolean): String {
+        if (old)
+            return when (oldColor) {
+                TextColor.RED -> "§c"
+                TextColor.DARK_RED -> "§4"
+                TextColor.GREEN -> "§a"
+                TextColor.DARK_GREEN -> "§2"
+                TextColor.BLUE -> "§9"
+                TextColor.DARK_BLUE -> "§1"
+                TextColor.AQUA -> "§3"
+                TextColor.PURPLE -> "§d"
+                TextColor.DARK_PURPLE -> "§5"
+                TextColor.GOLD -> "§6"
+                TextColor.YELLOW -> "§e"
+                TextColor.GRAY -> "§7"
+                TextColor.DARK_GRAY -> "§8"
+                TextColor.BLACK -> "§0"
+                TextColor.WHITE -> "§f"
+            }
+
+        return when (chatColor) {
+            TextColor.RED -> "§c"
+            TextColor.DARK_RED -> "§4"
+            TextColor.GREEN -> "§a"
+            TextColor.DARK_GREEN -> "§2"
+            TextColor.BLUE -> "§9"
+            TextColor.DARK_BLUE -> "§1"
+            TextColor.AQUA -> "§3"
+            TextColor.PURPLE -> "§d"
+            TextColor.DARK_PURPLE -> "§5"
+            TextColor.GOLD -> "§6"
+            TextColor.YELLOW -> "§e"
+            TextColor.GRAY -> "§7"
+            TextColor.DARK_GRAY -> "§8"
+            TextColor.BLACK -> "§0"
+            TextColor.WHITE -> "§f"
+        }
+    }
 
     private fun SafeClientEvent.getSurroundingSigns(playerPos: BlockPos): ArrayList<BlockPos> {
         val signList = ArrayList<BlockPos>()
@@ -88,8 +136,8 @@ internal object ChatSigns : PluginModule(
 
             val old = (oldSigns && nbt != null && OldSigns.isOld("$nbt"))
             var chatData = when {
-                old -> " §8[§cOLD§8]§f:\n§2§o\"${textOnSignForChat.trimEnd().replace("\n", "\n§2")}\""
-                else -> "\n§o\"${textOnSignForChat.trimEnd()}\""
+                old -> " §8[§cOLD§8]§f:\n${color(true)}§o\"${textOnSignForChat.trimEnd().replace("\n", "\n${color(true)}")}\""
+                else -> "\n§o${color(false)}\"${textOnSignForChat.trimEnd().replace("\n", "\n${color(false)}")}\""
             }
             if (displayMode == DisplayMode.COMPACT) chatData = chatData.replace("\n", " ")
             val coords = if (showCoords) " [§f${signX}§8, §f${signY}§8, §f${signZ}§8]§f: " else " "
